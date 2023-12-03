72d86b7104f1d5dc041dbfd6acd0f9ca9dcf3cc6
Simon Meister <simon.meister@wagner.ch>
2023-12-03 19:00:11 +0000

    update devcontainer

--- .devcontainer/symlink.sh
+++ .devcontainer/symlink.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-rm -rfv $OMD_ROOT/local/lib/python3/labelpicker
+for DIR in 'agents' 'checkman' 'checks' 'doc' 'inventory' 'notifications' 'pnp-templates' 'web'; do
+    rm -rfv $OMD_ROOT/local/share/check_mk/$DIR
+    ln -sv $WORKSPACE/$DIR $OMD_ROOT/local/share/check_mk/$DIR
+done;
+
+mkdir -p $OMD_ROOT/local/lib/check_mk/gui/plugins/wato/
+mkdir -p $OMD_ROOT/local/lib/check_mk/gui/plugins/wato/check_parameters
+ln -sv $WORKSPACE/agent_based_check_parameters $OMD_ROOT/local/lib/check_mk/gui/plugins/wato/check_parameters
+
 ln -sv $WORKSPACE/lib/python3/labelpicker $OMD_ROOT/local/lib/python3/labelpicker
 
 rm -rfv $OMD_ROOT/local/bin
